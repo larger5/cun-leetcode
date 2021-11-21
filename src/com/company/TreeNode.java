cbb23debbc13d75977debc20b04648d556b30819
larger5 <354073878@qq.com>
2021-11-21 18:51:47 +0800

    二叉树树节点

---
+++ src/com/company/TreeNode.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ * 二叉树树节点
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
